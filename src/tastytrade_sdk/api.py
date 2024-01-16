d8de7311256dca880b03d9d0ae81c424a4544a0c
Restyled.io <commits@restyled.io>
2024-01-16 20:54:36 +0000

    Restyled by black

--- src/tastytrade_sdk/api.py
+++ src/tastytrade_sdk/api.py
@@ -13,25 +13,29 @@ QueryParams = Union[Dict[str, Any], List[Tuple[str, Any]]]
 @singleton
 class RequestsSession:
     __session = Session()
-    __user_agent = 'tastytrade-sdk-python'
+    __user_agent = "tastytrade-sdk-python"
 
     @inject
     def __init__(self, config: Config):
-        self.__base_url = f'https://{config.api_base_url}'
+        self.__base_url = f"https://{config.api_base_url}"
 
     def login(self, login: str, password: str) -> None:
-        self.__session.headers['Authorization'] = self.request(
-            'POST',
-            '/sessions',
-            data={'login': login, 'password': password}
-        )['data']['session-token']
+        self.__session.headers["Authorization"] = self.request(
+            "POST", "/sessions", data={"login": login, "password": password}
+        )["data"]["session-token"]
 
-    def request(self, method: str, path: str, params: Optional[QueryParams] = tuple(),
-                data: Optional[dict] = None) -> Optional[dict]:
+    def request(
+        self,
+        method: str,
+        path: str,
+        params: Optional[QueryParams] = tuple(),
+        data: Optional[dict] = None,
+    ) -> Optional[dict]:
         url = self.__url(path, params)
-        logging.debug('%s %s', path, params)
-        response = self.__session.request(method, url, json=data, headers={
-                                          'User-Agent': self.__user_agent})
+        logging.debug("%s %s", path, params)
+        response = self.__session.request(
+            method, url, json=data, headers={"User-Agent": self.__user_agent}
+        )
         is_ok = 200 <= response.status_code <= 399
         if is_ok:
             try:
@@ -47,11 +51,11 @@ class RequestsSession:
         raise Unknown()
 
     def __url(self, path: str, params: Optional[QueryParams] = None) -> str:
-        url = f'{self.__base_url}{path}'
+        url = f"{self.__base_url}{path}"
         if params:
             if isinstance(params, dict):
                 params = list(params.items())
-            url += '?' + '&'.join(f'{p[0]}={p[1]}' for p in params)
+            url += "?" + "&".join(f"{p[0]}={p[1]}" for p in params)
         return url
 
 
@@ -81,40 +85,55 @@ class Api:
 
     def get(self, path: str, params: Optional[QueryParams] = None) -> Optional[dict]:
         """Make a GET request"""
-        return self.__session.request('GET', path, params=params)
+        return self.__session.request("GET", path, params=params)
 
-    def post(self, path: str, params: Optional[QueryParams] = None, data: Optional[dict] = None) -> Optional[dict]:
+    def post(
+        self,
+        path: str,
+        params: Optional[QueryParams] = None,
+        data: Optional[dict] = None,
+    ) -> Optional[dict]:
         """Make a POST request"""
-        return self.__session.request('POST', path, params=params, data=data)
+        return self.__session.request("POST", path, params=params, data=data)
 
-    def put(self, path: str, params: Optional[QueryParams] = None, data: Optional[dict] = None) -> Optional[dict]:
+    def put(
+        self,
+        path: str,
+        params: Optional[QueryParams] = None,
+        data: Optional[dict] = None,
+    ) -> Optional[dict]:
         """Make a PUT request"""
-        return self.__session.request('PUT', path, params=params, data=data)
+        return self.__session.request("PUT", path, params=params, data=data)
 
-    def patch(self, path: str, params: Optional[QueryParams] = None, data: Optional[dict] = None) -> Optional[dict]:
+    def patch(
+        self,
+        path: str,
+        params: Optional[QueryParams] = None,
+        data: Optional[dict] = None,
+    ) -> Optional[dict]:
         """Make a PATCH request"""
-        return self.__session.request('PATCH', path, params=params, data=data)
+        return self.__session.request("PATCH", path, params=params, data=data)
 
     def delete(self, path: str, params: Optional[QueryParams] = None) -> Optional[dict]:
         """Make a DELETE request"""
-        return self.__session.request('DELETE', path, params=params)
+        return self.__session.request("DELETE", path, params=params)
 
 
 class Unauthorized(TastytradeSdkException):
     def __init__(self):
-        super().__init__('Unauthorized')
+        super().__init__("Unauthorized")
 
 
 class BadRequest(TastytradeSdkException):
     def __init__(self):
-        super().__init__('Bad Request')
+        super().__init__("Bad Request")
 
 
 class ServerError(TastytradeSdkException):
     def __init__(self):
-        super().__init__('Server Error')
+        super().__init__("Server Error")
 
 
 class Unknown(TastytradeSdkException):
     def __init__(self):
-        super().__init__('Unknown Error')
+        super().__init__("Unknown Error")
